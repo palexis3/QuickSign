fadfccc8491ceef1568a00237472ae1d30895cf5
palexis3 <alexispatrick77@gmail.com>
2017-05-02 16:49:36 -0700

    Creating models

---
+++ app/src/main/java/com/example/palexis3/quicksign/Models/SectionTemplates.java
@@ -0,0 +1,5 @@
+package com.example.palexis3.quicksign.Models;
+
+
+public class SectionTemplates {
+}
--- app/src/main/java/com/example/palexis3/quicksign/Models/Templates.java
+++ app/src/main/java/com/example/palexis3/quicksign/Models/Templates.java
@@ -1,6 +1,13 @@
 package com.example.palexis3.quicksign.Models;
 
 
-
 public class Templates {
+
+    private String name;
+    private String imageUrl;
+    private String description;
+    private String text;
+
+    
+
 }
